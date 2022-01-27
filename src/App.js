d2040695d73b34b2a82f83fcbaa64d24533dc3eb
liisapikkarainen <liisapikkarainen1@gmail.com>
2022-01-27 19:31:42 +0200

    List-tehtävä

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [rivit, setRivit] = useState([]);
+
+  useEffect(() => {
+    const nimet = ['Jouni', 'Pekka', 'Sinikka', 'Teppo', 'Meija', 'Reima'];
+    setRivit(nimet);
+    /* console.log("useEffect");
+    console.log(nimet); */
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{margin: '30px'}}>
+      <h3>Nimet</h3>
+      <ul>
+        {
+          rivit.map(rivi => (
+            <li key={rivi}>{rivi}</li>
+          ))
+        }
+      </ul>
     </div>
   );
 }
